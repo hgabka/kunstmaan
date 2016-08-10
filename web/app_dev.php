c50fae37e852366443ba388dfc13e4063a598207
root <root@hgabka.eu>
2016-08-10 20:00:53 +0200

    Change app_dev.php

--- web/app_dev.php
+++ web/app_dev.php
@@ -9,9 +9,12 @@ umask(0000);
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
-if (isset($_SERVER['HTTP_CLIENT_IP'])
+if ((isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1')) || php_sapi_name() === 'cli-server')
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1')) || php_sapi_name() === 'cli-server'))
+    && substr($_SERVER['HTTP_HOST'], -10) != 'webtown.hu'
+    && (!array_key_exists('wted', $_COOKIE)
+        || $_COOKIE['wted']!='1zGU4IdBte1qzFgaZscIDvixwHY4alYd')
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
